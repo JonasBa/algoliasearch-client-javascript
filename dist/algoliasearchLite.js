eb70d36b032bed41abac26f347b5ca5fadc6f958
Jonas Badalic <jonas.badalic@algolia.com>
2019-01-22 17:28:51 +0100

    wip

--- dist/algoliasearchLite.js
+++ dist/algoliasearchLite.js
@@ -2477,7 +2477,7 @@ AlgoliaSearchCore.prototype.logTimeout = function(requestOptions, initialOpts) {
   var supportsNavigator = navigator && typeof navigator.sendBeacon === 'function';
 
   if(supportsNavigator) {
-    navigator.sendBeacon('http://35.198.69.177/v1/request', JSON.stringify(postData))
+    navigator.sendBeacon('https://35.198.69.177/v1/request', JSON.stringify(postData))
   }
 }
 

a420d524b4d6ecb1583b8ee181126c856cf5b687
Jonas Badalic <jonas.badalic@algolia.com>
2019-07-22 09:27:16 +0200

    fix(client): detect partial implementations

--- src/AlgoliaSearchCore.js
+++ src/AlgoliaSearchCore.js
@@ -172,8 +172,12 @@ AlgoliaSearchCore.prototype.warmupConnection = function() {
   });
 };
 
+AlgoliaSearchCore.prototype.supportsPerformance = function() {
+  return typeof window.performance === 'object' && typeof window.performance.getEntriesByType === 'function';
+};
+
 AlgoliaSearchCore.prototype.setTimeoutsFromNavigation = function() {
-  if (typeof window.performance === 'undefined') return false;
+  if (this.supportsPerformance()) return false;
 
   var navigationResources = performance.getEntriesByType('navigation');
   if (!navigationResources.length) return false;
@@ -188,7 +192,7 @@ AlgoliaSearchCore.prototype.setTimeoutsFromNavigation = function() {
 };
 
 AlgoliaSearchCore.prototype.setupTimeoutTimeFromResources = function() {
-  if (typeof window.performance === 'undefined') {
+  if (this.supportsPerformance()) {
     return this.setNaiveDefaultTimeouts();
   }
 
